b811709359267dd8eb53bccbde02ea42a40a8676
kt5007 <kenjin.0546@gmail.com>
2021-06-06 08:07:39 +0900

    changed user edit function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -77,19 +77,37 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
+        
         $auth_user = Auth::user();
-
+        // バリデーション
         $rules = [
             'thumbnail' => 'file|mimes:jpeg,png,jpg,bmb|max:2048',
             'name' => ['required', 'max:100'],
             'email' => ['required'],
             'current_password' => ['required'],
-            'password' => [''],
-            'password_confirmation' => ['same:password'],
         ];
-        $validator = Validator::make($request->all(), $rules);
+        
+        if(isset($request->password)){
+            $rules['password']='min:8';
+            $rules['password_confirmation']='same:password';
+        }
+
+        $messages = [
+            'required' => ':attributeを入力してください',
+            'name.max' => '100字以下でお願いします',
+            'password.min' => '8文字以上でお願いします',
+            'password_confirmation.same' => 'パスワードが一致しません'
+        ];
+        $attributes = [
+            'name' => '名前',
+            'email' => 'メール',
+            'current_password' => '現在のパスワード',
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages, $attributes);
 
         // サムネイル画像の変更がある場合は格納
+        $uploadfile=$request->thumbnail;
         if (!empty($uploadfile)) {
             $thumbnailname = $uploadfile->hashName();
             $uploadfile->storeAs('public/user', $thumbnailname);
@@ -97,30 +115,50 @@ class UserController extends Controller
             $thumbnailname = $auth_user->thumbnail;
         }
 
+        if(isset($request->delete_thumbnail)){
+            $thumbnailname = null;
+        }
+
         // 現在のパスワードと一致しているか確認
         if (Hash::check($request->current_password, $auth_user->password)) {
-            
+            // エラーがある場合   
             if ($validator->fails()) {
                 return redirect()
                     ->route('user.edit', [$auth_user])
                     ->withErrors($validator)
                     ->withInput();
-            }else{
-                DB::table('users')
-                    ->where('id','=',$auth_user->id)
-                    ->update([
-                        'name' => $request->name,
-                        'email' => $request->email,
-                        'thumbnail' => $thumbnailname,
-                        'password' => Hash::make($request->password),
-                    ]);
-                $request->session()->flash('success', 'Password changed');
-                return redirect()->route('user.index')->with('success', 'Saved');
-            }
+            // うまくいった場合
+            } else {
+                $insert_data = [
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'thumbnail' => $thumbnailname,
+                ];
 
+                // パスワードの更新がある場合
+                if(isset($request->password)){
+                    $insert_password = $request->password;
+                    $insert_data['password'] = Hash::make($insert_password);
+                }
+                DB::table('users')
+                    ->where('id', '=', $auth_user->id)
+                    ->update($insert_data);
+                $request->session()->flash('success', 'Saved');
+                return redirect()->route('user.index');
+            }
+        // 現在のパスワードと一致しない場合
         } else {
-            $request->session()->flash('error', 'Current Password does not match');
-            return redirect()->route('user.edit',compact('auth_user'));
+            // with validation error
+            if ($validator->fails()) {
+                return redirect()
+                    ->route('user.edit', [$auth_user])
+                    ->withErrors($validator)
+                    ->withInput();
+                // no validation error
+            } else {
+                $request->session()->flash('error', 'Current Password does not match');
+                return redirect()->route('user.edit', 'auth_user');
+            }
         }
     }
 
